b679176b7c209c47ba65fd36e9d6851d3d45550c
Ishaq Hassan <ishaquehassan@gmail.com>
2022-01-08 16:30:46 +0500

    Class code of 8 Jan 2022

---
+++ 8Jan2022/list.dart
@@ -0,0 +1,5 @@
+void main() {
+  List<String> friends;
+  friends = ["Ishaq", "Abc", "Xyz"];
+  print(friends[2]);
+}
---
+++ 8Jan2022/vars.dart
@@ -0,0 +1,20 @@
+void main() {
+  int number1 = 2;
+  int number2 = 2;
+  int sum1;
+  sum1 = number1+number2;
+  print(sum1);
+  String name = "Ishaq";
+  // you can also write true here :D
+  bool isChaiArrived = false;
+  print(isChaiArrived);
+  double height = 5.1;
+  // We have oprators like + - * / %
+  print(number1%number2);
+  print(number1+height);
+  double sum = number1+height;
+  //print(sum);
+  var age = 12;
+  // this is invalid now age = "Ishaq";
+  
+} 
