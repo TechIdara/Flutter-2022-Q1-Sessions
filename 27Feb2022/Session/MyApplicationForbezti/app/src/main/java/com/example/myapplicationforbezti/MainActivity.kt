da2bb3d9582dce6c69bb3dc16d28f55c107c7e79
Ishaq Hassan <ishaquehassan@gmail.com>
2022-02-27 23:30:21 +0500

    - Updated Native android code for Bezti app

--- 27Feb2022/Session/MyApplicationForbezti/app/src/main/java/com/example/myapplicationforbezti/MainActivity.kt
+++ 27Feb2022/Session/MyApplicationForbezti/app/src/main/java/com/example/myapplicationforbezti/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplicationforbezti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.meraBtn).setOnClickListener(View.OnClickListener {
+
+            findViewById<TextView>(R.id.mera1stTextView).text = "";
+            findViewById<TextView>(R.id.mera1stTextView).text = "";
+            findViewById<TextView>(R.id.mera1stTextView).text = "";
+        })
     }
 }
